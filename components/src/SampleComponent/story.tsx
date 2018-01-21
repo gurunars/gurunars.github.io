5bff4e538998ef28f7ef9959f0fa0a09ee75f4fa
Anton Berezin <anton.berezin@nephilim.solutions>
2018-01-21 18:05:57 +0200

    Index.tsx

--- components/src/SampleComponent/story.tsx
+++ components/src/SampleComponent/story.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Foo from './Foo';
+import Foo from '.';
 
 storiesOf('Button', module)
   .add('basic', () => (
