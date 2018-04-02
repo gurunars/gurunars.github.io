2b3970c6c186e0143c12fb6c0f9435777c17bee3
Anton Berezin <anton.berezin@nephilim.solutions>
2018-04-02 19:44:15 +0300

    ff

---
+++ frontend/src/Link/icons/gen_index.py
@@ -0,0 +1,39 @@
+import os
+
+def get_icons():
+    for fil in os.listdir("."):
+        if not fil.endswith(".svg"):
+            continue
+        yield fil.replace(".svg", "")
+
+icons = list(get_icons())
+
+imports = []
+dicts = []
+
+for icon in icons:
+    imports.append(
+        'const {name} = require("./{name}.svg)'.format(name=icon)
+    )
+    dicts.append(
+        '{name}: {name}'.format(name=icon)
+    )
+
+text = """
+const fallback = require("./link.svg");
+
+{imports}
+
+const icons = {{
+    {dicts}
+}};
+
+const getIconForType = (type: string) =>
+    icons[type] || fallback; 
+""".format(
+    imports=";\n".join(imports),
+    dicts=",\n    ".join(dicts)
+)
+
+with open("index.tsx", "w") as fil:
+    fil.write(text)
---
+++ frontend/src/Link/icons/index.tsx
@@ -0,0 +1,35 @@
+
+const fallback = require("./link.svg");
+
+const skype = require("./skype.svg);
+const pdf = require("./pdf.svg);
+const phone = require("./phone.svg);
+const email = require("./email.svg);
+const docs = require("./docs.svg);
+const cv = require("./cv.svg);
+const linkedin = require("./linkedin.svg);
+const stackoverflow = require("./stackoverflow.svg);
+const amazon = require("./amazon.svg);
+const play = require("./play.svg);
+const link = require("./link.svg);
+const github = require("./github.svg);
+const print = require("./print.svg)
+
+const icons = {
+    skype: skype,
+    pdf: pdf,
+    phone: phone,
+    email: email,
+    docs: docs,
+    cv: cv,
+    linkedin: linkedin,
+    stackoverflow: stackoverflow,
+    amazon: amazon,
+    play: play,
+    link: link,
+    github: github,
+    print: print
+};
+
+const getIconForType = (type: string) =>
+    icons[type] || fallback; 
