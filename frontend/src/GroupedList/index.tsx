b65ab9d2b848943b97fcbad2c2c83eab6a972d81
Anton Berezin <anton.berezin@nephilim.solutions>
2018-04-03 01:16:12 +0300

    ff

--- frontend/src/GroupedList/index.tsx
+++ frontend/src/GroupedList/index.tsx
@@ -1,40 +1,29 @@
 import * as React from "react";
 
 import { GroupedItems } from "./grouping";
+import responsive from "../Responsive";
 
 type RenderItem<T extends {}> = (props: {item: T}) => React.ReactElement<any>;
 
-const Group = <T extends {}>(props: {
-  group: GroupedItems<T>, 
-  renderItem: RenderItem<T>
-}): React.ReactElement<any> => (
-  <div 
-    style={{
-      marginBottom: 15,
-      pageBreakInside: "avoid"
-    }}
-  >
-    <h2 style={{ pageBreakAfter: "avoid" }}>{props.group.group}</h2>
-    <div 
-      style={{
-        display: "flex"
-      }}
-    >
-    {props.group.elements.map(item => props.renderItem({item: item}))}
-    </div>
-  </div>
-);
-
 const GroupedList = <T extends {}>(props: {
   items: GroupedItems<T>[],
-  renderItem: RenderItem<T>
+  renderItem: RenderItem<T>,
+  style?: React.CSSProperties
 }): React.ReactElement<any> => (
   <div>
-    {props.items.map(item => <Group 
-      key={item.group} 
-      group={item} 
-      renderItem={props.renderItem} 
-    />)}
+    {props.items.map(item => (
+    <div 
+      style={{
+        marginBottom: 15,
+        pageBreakInside: "avoid"
+      }}
+    >
+      <h2 style={{ pageBreakAfter: "avoid" }}>{item.group}</h2>
+      <div style={props.style}>
+        {item.elements.map(element => props.renderItem({item: element}))}
+      </div>
+    </div>
+    ))}
   </div>
 );
 
@@ -44,6 +33,7 @@ export const Desktop = <T extends {}>(props: {
 }): React.ReactElement<any> => (
   <GroupedList
     items={props.items}
+    style={{display: "flex"}}
     renderItem={ item => (
       <div 
         style={{
@@ -59,4 +49,28 @@ export const Desktop = <T extends {}>(props: {
   />
 );
 
-export default Desktop;
+export const Mobile = <T extends {}>(props: {
+  items: GroupedItems<T>[],
+  renderItem: RenderItem<T>
+}): React.ReactElement<any> => (
+  <GroupedList
+    items={props.items}
+    renderItem={ item => (
+      <div 
+        style={{
+          display: "inline-block",
+          position: "relative",
+          width: "100%",
+          height: "auto"
+        }}
+      >
+        {props.renderItem(item)}
+      </div>
+    )}
+  />
+);
+
+export default responsive({
+  desktopView: Desktop,
+  mobileView: Mobile
+});
--- frontend/src/GroupedList/story.tsx
+++ frontend/src/GroupedList/story.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { host } from "storybook-host";
 
-import { Desktop } from ".";
+import { Desktop, Mobile } from ".";
 import { groupItems } from "./grouping";
 
 interface Item {
@@ -28,7 +28,13 @@ storiesOf("GroupedList", module)
     height: 600,
     width: 800,
   }))
-  .add("basic", () => (
+  .add("mobile", () => (
+    <Mobile 
+      items={groupItems(items, "type", "title")} 
+      renderItem={(props: {item: Item}) => <h1>{props.item.title}</h1>} 
+    />
+  ))
+  .add("desktop", () => (
     <Desktop 
       items={groupItems(items, "type", "title")} 
       renderItem={(props: {item: Item}) => <h1>{props.item.title}</h1>} 
