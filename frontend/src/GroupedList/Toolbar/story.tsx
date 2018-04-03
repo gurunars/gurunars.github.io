7575d069b80224d7498933c94412e57922f5fd08
Anton Berezin <anton.berezin@nephilim.solutions>
2018-04-03 22:18:14 +0300

    ff

--- frontend/src/GroupedList/Toolbar/story.tsx
+++ frontend/src/GroupedList/Toolbar/story.tsx
@@ -1,14 +1,28 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { host } from "storybook-host";
-import { withState } from "recompose";
+import { withState, compose, withProps } from "recompose";
 
 import { SpecFilter } from ".";
 
-const StateSpecFilter = withState(
-  "selectedSpecs",
-  "selectedSpecsOnChange",
-  []
+const StateSpecFilter = compose(
+  withProps({
+    "mapping": {
+      one: {
+        humanReadableName: "One",
+        color: "Red"
+      },
+      two: {
+        humanReadableName: "Two",
+        color: "Blue"
+      }
+    }
+  }),
+  withState(
+    "selectedSpecs",
+    "selectedSpecsOnChange",
+    []
+  )
 )(SpecFilter);
 
 storiesOf("Toolbar", module)
@@ -18,5 +32,5 @@ storiesOf("Toolbar", module)
     width: 800,
   }))
   .add("SpecFilter", () => (
-    <StateSpecFilter /> 
+    <StateSpecFilter />
   )); 
