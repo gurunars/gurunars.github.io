810297807ce6835a262754934c5bb28c7bc6a144
Anton Berezin <anton.berezin@nephilim.solutions>
2018-04-06 01:13:58 +0300

    ff

--- frontend/src/About/index.tsx
+++ frontend/src/About/index.tsx
@@ -12,89 +12,95 @@ export interface Meta {
   media: Link[];
 }
 
-const About = ({ meta, color }: { meta: Meta, color?: string }): React.ReactElement<any> => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "flex-start",
-      height: "100%",
-      maxWidth: 250,
-      color: color || "black"
-    }}
-  >
-    <div
-      style={{
-        width: 250,
-        height: 80,
-        alignItems: "center",
-        padding: 10,
-        display: "flex"
-      }}
-    >
-      <img
-        src={meta.avatar}
-        style={{
-          width: 65,
-          height: 65,
-          marginRight: 10,
-          borderRadius: "50%",
-          border: "5px solid black"
-        }}
-      />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column"
-        }}
-      >
-        <span style={{ fontSize: 24 }}>{meta.name}</span>
-        <i>{meta.birthday}</i>
-        <div style={{ marginTop: 5 }}>
-          {_.join(meta.languages, " | ")}
-        </div>
-      </div>
-    </div>
-
-    <div
-      style={{
-        display: "block",
-        width: 250,
-        marginTop: 10,
-        paddingTop: 10,
-        paddingBottom: 10,
-        paddingLeft: 5,
-        paddingRight: 5,
-        borderTop: "1px solid grey",
-      }}
-    >
-      {meta.specialization.map(spec => (
-        <p key={spec} style={{ paddingTop: 6 }}>
-          {spec}
-        </p>
-      ))}
-    </div>
-
+const About = ({ meta, primaryColor, secondaryColor }: {
+  meta: Meta, primaryColor: string, secondaryColor: string
+}): React.ReactElement<any> => (
     <div
       style={{
         display: "flex",
-        borderTop: "1px solid grey",
         flexDirection: "column",
+        alignItems: "center",
         justifyContent: "flex-start",
-        width: 250,
-        paddingTop: 10,
-        paddingBottom: 10,
-        marginTop: 10,
-        marginBottom: 10
+        height: "100%",
+        maxWidth: 250,
+        color: primaryColor || "black"
       }}
     >
-      {meta.media.map(media =>
-        <FullUrl style={{ color: color || "black" }} key={media.name} link={media} />
-      )}
-    </div>
+      <div
+        style={{
+          width: 250,
+          height: 80,
+          alignItems: "center",
+          padding: 10,
+          display: "flex"
+        }}
+      >
+        <img
+          src={meta.avatar}
+          style={{
+            width: 65,
+            height: 65,
+            marginRight: 10,
+            borderRadius: "50%",
+            border: "5px solid black"
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column"
+          }}
+        >
+          <span style={{ fontSize: 24 }}>{meta.name}</span>
+          <i>{meta.birthday}</i>
+          <div style={{ marginTop: 5 }}>
+            {_.join(meta.languages, " | ")}
+          </div>
+        </div>
+      </div>
 
-  </div>
-);
+      <div
+        style={{
+          display: "block",
+          width: 250,
+          marginTop: 10,
+          paddingTop: 10,
+          paddingBottom: 10,
+          paddingLeft: 5,
+          paddingRight: 5,
+          borderTop: "1px solid grey",
+        }}
+      >
+        {meta.specialization.map(spec => (
+          <p key={spec} style={{ paddingTop: 6 }}>
+            {spec}
+          </p>
+        ))}
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          borderTop: "1px solid grey",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          width: 250,
+          paddingTop: 10,
+          paddingBottom: 10,
+          marginTop: 10,
+          marginBottom: 10
+        }}
+      >
+        {meta.media.map(media =>
+          <FullUrl
+            style={{ color: secondaryColor || "black", textDecoration: "none" }}
+            key={media.name}
+            link={media}
+          />
+        )}
+      </div>
+
+    </div>
+  );
 
 export default About;
