fa417235ee5e4ace9478a04e7a75fcbfb1bc5b30
Anton Berezin <anton.berezin@nephilim.solutions>
2018-04-06 00:18:15 +0300

    ff

--- frontend/src/Link/icons/gen_index.py
+++ frontend/src/Link/icons/gen_index.py
@@ -1,6 +1,8 @@
 import os
+from scour.scour import run, start
 
 # TODO: run this script on build
+# scour --enable-viewboxing --remove-metadata --enable-id-stripping -i stackoverflow.svg -o stackoverflow.svg.new
 
 
 def get_icons():
@@ -12,10 +14,24 @@ def get_icons():
 
 icons = list(get_icons())
 
+
+def cleanup(icon):
+    options, (input, output) = parse_args([
+        "--enable-viewboxing",
+        "--remove-metadata",
+        "--enable-id-stripping",
+        "-i", icon, "-o", icon + ".new"
+    ])
+    start(options, input, output)
+    os.remove(icon)
+    os.rename(icon + ".new", icon)
+
+
 imports = []
 dicts = []
 
 for icon in icons:
+    cleanup(icon)
     imports.append(
         'const {name} = require("./{name}.svg")'.format(name=icon)
     )
