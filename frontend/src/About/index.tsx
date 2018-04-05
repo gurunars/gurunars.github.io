70e3fb1a08592cc9fd87e00da64378fe10ce483a
Anton Berezin <anton.berezin@nephilim.solutions>
2018-04-05 23:19:34 +0300

    Export meta

--- frontend/src/About/index.tsx
+++ frontend/src/About/index.tsx
@@ -1,100 +1,99 @@
-/*
 import * as React from "react";
+import * as _ from "lodash";
 
-import { merge } from "../utils";
+import { FullUrl, Link } from "../Link";
 
-const About = ({ meta, contactIconColor, textColor }: {
-  meta: Meta,
-  contactIconColor: string,
-  textColor: string
-}): React.ReactElement<any> => (
+export interface Meta {
+  name: string;
+  languages: string[];
+  birthday: string;
+  specialization: string[];
+  avatar: string;
+  media: Link[];
+}
+
+const About = ({ meta }: { meta: Meta }): React.ReactElement<any> => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      height: "100%",
+      maxWidth: 250
+    }}
+  >
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
+        width: 250,
+        height: 80,
         alignItems: "center",
-        justifyContent: "flex-start",
-        height: "100%",
-        maxWidth: 250
+        padding: 10,
+        display: "flex"
       }}
     >
-      <div
+      <img
+        src={meta.avatar}
         style={{
-          width: 250,
-          height: 80,
-          alignItems: "center",
-          padding: 10,
-          display: "flex"
+          width: 65,
+          height: 65,
+          marginRight: 10,
+          borderRadius: "50%",
+          border: "5px solid black"
         }}
-      >
-        <img
-          src={meta.avatar}
-          style={{
-            width: 65,
-            height: 65,
-            marginRight: 10,
-            borderRadius: "50%",
-            border: "5px solid black"
-          }}
-        />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column"
-          }}
-        >
-          <span style={{ fontSize: 24, color: textColor }}>{meta.name}</span>
-          <i style={{ color: textColor }}>{meta.birthday}</i>
-          <div style={{ marginTop: 5 }}>
-            {flags}
-          </div>
-        </div>
-      </div>
-
-      <div
-        style={{
-          display: "block",
-          width: 250,
-          marginTop: 10,
-          paddingTop: 10,
-          paddingBottom: 10,
-          paddingLeft: 5,
-          paddingRight: 5,
-          borderTop: "1px solid grey",
-        }}
-      >
-        {meta.specialization.map(spec => (
-          <p
-            key={spec}
-            style={{
-              paddingTop: 6,
-              color: textColor
-            }}
-          >
-            {spec}
-          </p>
-        )})}
-      </div>
-
+      />
       <div
         style={{
           display: "flex",
-          borderTop: "1px solid grey",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          width: 250,
-          paddingTop: 10,
-          paddingBottom: 10,
-          marginTop: 10,
-          marginBottom: 10
+          flexDirection: "column"
         }}
       >
-        {meta.media.map(media =>
-          <ComIcon key={media} color={contactIconColor} link={media} />
-        })}
+        <span style={{ fontSize: 24 }}>{meta.name}</span>
+        <i>{meta.birthday}</i>
+        <div style={{ marginTop: 5 }}>
+          {_.join(meta.languages, " | ")}
+        </div>
       </div>
-
     </div>
-  );
 
-*/
+    <div
+      style={{
+        display: "block",
+        width: 250,
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 5,
+        paddingRight: 5,
+        borderTop: "1px solid grey",
+      }}
+    >
+      {meta.specialization.map(spec => (
+        <p key={spec} style={{ paddingTop: 6 }}>
+          {spec}
+        </p>
+      ))}
+    </div>
+
+    <div
+      style={{
+        display: "flex",
+        borderTop: "1px solid grey",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        width: 250,
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: 10,
+        marginBottom: 10
+      }}
+    >
+      {meta.media.map(media =>
+        <FullUrl key={media.name} link={media} />
+      )}
+    </div>
+
+  </div>
+);
+
+export default About;
--- frontend/src/About/story.tsx
+++ frontend/src/About/story.tsx
@@ -1,10 +1,24 @@
-/*
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import { host } from "storybook-host";
 
 import About from ".";
+
+const META = {
+  name: "John Wick",
+  languages: ["EN", "RU"],
+  birthday: "24.03.1959",
+  specialization: ["Shoot", "Steal", "Blackmail"],
+  avatar: "https://img-www.tf-cdn.com/movie/2/john-wick-chapter-two-2017" +
+    ".jpeg?_v=20170202222435&fit=crop&crop=faces%20top&w=400&h=400",
+  media: [
+    {
+      type: "amazon",
+      url: "wherever",
+      name: "Ama Zone"
+    }
+  ]
+};
 
 storiesOf("About", module)
   .addDecorator(host({
@@ -13,9 +27,5 @@ storiesOf("About", module)
     width: 800,
   }))
   .add("basic", () => (
-    <About 
-      title="Sample Title" 
-      onClick={action("onClick")} />
+    <About meta={META} />
   )); 
-
-  */
