acf8480a360c28a5581a452fac37f686cd88c9ff
Anton Berezin <anton.berezin@nephilim.solutions>
2018-04-08 00:20:53 +0300

    Added a hashsum caculating function

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -1,2 +1,14 @@
 export const merge = (...sources: Object[]): Object =>
   Object.assign({}, ...sources);
+
+export const hashCode = (str: string): number => {
+    let hash = 0;
+    if (str.length === 0) { return hash; }
+    for (let i = 0; i < str.length; i++) {
+        let char = str.charCodeAt(i);
+        // @ts-ignore
+        hash = (( hash << 5) - hash) + char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+};
