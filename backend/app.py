ba142db4e618e94b77f45e1585e0208dd533f88b
Anton Berezin <anton.berezin@nephilim.solutions>
2018-04-09 00:42:44 +0300

    moves

--- backend/app.py
+++
@@ -1,9 +0,0 @@
-from werkzeug.wrappers import Response
-
-def application(environ, start_response):
-    response = Response('Hello World!', mimetype='text/plain')
-    return response(environ, start_response)
-
-if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-    run_simple('127.0.0.1', 5000, application, use_debugger=True, use_reloader=True)
