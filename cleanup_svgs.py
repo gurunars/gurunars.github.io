c61129fd7a5782de571dbf920f0ce04b9ac00f18
Anton Berezin <anton.berezin@nephilim.solutions>
2018-07-30 19:06:15 +0300

    ff

--- cleanup_svgs.py
+++ cleanup_svgs.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from scour.scour import parse_args, start, getInOut
 from xml.etree.ElementTree import ElementTree, register_namespace
 
@@ -6,23 +7,12 @@ from xml.etree.ElementTree import ElementTree, register_namespace
 # scour --enable-viewboxing --remove-metadata --enable-id-stripping -i stackoverflow.svg -o stackoverflow.svg.new
 
 
-def get_icons():
-    for fil in os.listdir("./raw"):
-        if not fil.endswith(".svg"):
-            continue
-        yield fil.replace(".svg", "")
-
-
-icons = list(get_icons())
-
-
 def drop(svg, key):
     if key in svg.attrib:
         del svg.attrib[key]
 
 
-def cleanup(icon):
-    source = icon + ".svg"
+def cleanup(source):
     target = source + ".tmp"
 
     options = parse_args([
@@ -52,4 +42,5 @@ def cleanup(icon):
     tree.write(source)
 
 
-for icon in icons:
+for filename in glob.iglob('src/**/*.svg', recursive=True):
+    cleanup(filename)
