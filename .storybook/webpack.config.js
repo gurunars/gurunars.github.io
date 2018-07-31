e0edad3830d5eceb217ecf9a484752fa9ad0cbc4
Anton Berezin <anton.berezin@nephilim.solutions>
2018-07-31 20:32:01 +0300

    Added proper file loader for the icons

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,20 @@
 const path = require("path");
 
 module.exports = (baseConfig, env, defaultConfig) => {
-  // Extend defaultConfig as you need.
 
-  // For example, add typescript loader:
+  defaultConfig.module.rules = defaultConfig.module.rules.filter(it => String(it.test).indexOf("svg") == -1);
+
   defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
     include: path.resolve(__dirname, "../src"),
     loader: require.resolve("ts-loader")
   });
+
+  defaultConfig.module.rules.push({
+    test: /\.svg$/,
+    loader: "file-loader"
+  });
+
   defaultConfig.resolve.extensions.push(".ts", ".tsx");
 
   return defaultConfig;
