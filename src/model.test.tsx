ae30bb60c8ebb852381b8987e98f4033bd8d6512
Anton Berezin <gurunars@gmail.com>
2019-05-26 14:52:38 +0300

    Fixed unit tests

--- src/model.test.tsx
+++ src/model.test.tsx
@@ -3,24 +3,24 @@ import { extractTags } from "./model";
 describe("extractTags", () => {
   it("should extract and reformat both the curly wrapped and plain tags", () => {
     expect(extractTags("one #{two} #{three four} five #six seven")).toEqual([
-      "one #{two} #{three four} five #{six} seven",
+      "one two three four five six seven",
       ["two", "three four", "six"]
     ]);
   });
 
   it("should iclude extra hashes into the tag value", () => {
-    expect(extractTags("one ###hash")).toEqual(["one #{##hash}", ["##hash"]]);
+    expect(extractTags("one ###hash")).toEqual(["one ##hash", ["##hash"]]);
   });
 
   it("should include extra left curly parantheses", () => {
-    expect(extractTags("one #{{{foo}")).toEqual(["one #{{{foo}", ["{{foo"]]);
+    expect(extractTags("one #{{{foo}")).toEqual(["one {{foo", ["{{foo"]]);
   });
 
   it("should exclude extra right curly parantheses", () => {
-    expect(extractTags("one #{foo}}}}")).toEqual(["one #{foo}}}}", ["foo"]]);
+    expect(extractTags("one #{foo}}}}")).toEqual(["one foo}}}", ["foo"]]);
   });
 
   it("should include the whole string if the end is unbalanced", () => {
-    expect(extractTags("one #{foo")).toEqual(["one #{foo}", ["foo"]]);
+    expect(extractTags("one #{foo")).toEqual(["one foo", ["foo"]]);
   });
 });
