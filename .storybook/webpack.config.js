376900d2315ca63c505bdef161c070fdc8fe7a29
Anton Berezin <gurunars@gmail.com>
2019-05-26 14:37:39 +0300

    Storybook 5.0

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,21 +1,22 @@
 const path = require("path");
 
-module.exports = (baseConfig, env, defaultConfig) => {
+module.exports = ({ config }) => {
+  config.module.rules = config.module.rules.filter(
+    it => String(it.test).indexOf("svg") == -1
+  );
 
-  defaultConfig.module.rules = defaultConfig.module.rules.filter(it => String(it.test).indexOf("svg") == -1);
-
-  defaultConfig.module.rules.push({
+  config.module.rules.push({
     test: /\.(ts|tsx)$/,
     include: path.resolve(__dirname, "../src"),
     loader: require.resolve("ts-loader")
   });
 
-  defaultConfig.module.rules.push({
+  config.module.rules.push({
     test: /\.svg$/,
     loader: "file-loader"
   });
 
-  defaultConfig.resolve.extensions.push(".ts", ".tsx");
+  config.resolve.extensions.push(".ts", ".tsx");
 
-  return defaultConfig;
+  return config;
 };
