3be5ab0ef5ba5adb2f8136a8dabc18b4321779f0
Anton Berezin <gurunars@gmail.com>
2019-07-31 13:24:33 +0300

    Version updates

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,13 +4,12 @@ module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     include: path.resolve(__dirname, "../src"),
-    loader: require.resolve("babel-loader"),
+    loader: require.resolve("awesome-typescript-loader"),
     options: {
+      configFileName: "tsconfig.json",
       presets: [["react-app", { flow: false, typescript: true }]]
     }
   });
-
   config.resolve.extensions.push(".ts", ".tsx");
-
   return config;
 };
