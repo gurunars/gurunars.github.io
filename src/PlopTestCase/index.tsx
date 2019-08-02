3f417171e8ed008fe1dbbb3214d65dcbc8c18fa8
Anton Berezin <gurunars@gmail.com>
2019-08-02 15:40:48 +0300

    Cool rotations with pose

--- src/PlopTestCase/index.tsx
+++ src/PlopTestCase/index.tsx
@@ -1,36 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import posed from "react-pose";
 
 const Box = posed.div({
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    scaleY: 1,
+  open: {
+    rotate: 360 + 45,
     transition: {
-      opacity: { ease: "easeOut", duration: 300 },
-      default: { ease: "linear", duration: 500 }
+      duration: 400
+    }
+  },
+  closed: {
+    rotate: 0,
+    transition: {
+      duration: 400
     }
   }
 });
 
 const PlopTestCase = (): React.ReactElement<any> => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsVisible(!isVisible);
-    }, 1000);
-  });
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Box
+      onClick={() => setIsOpen(!isOpen)}
       style={{
         width: 100,
         height: 100,
         background: "#ff1c68",
         transformOrigin: "50% 50%"
       }}
-      pose={isVisible ? "visible" : "hidden"}
+      pose={isOpen ? "open" : "closed"}
     />
   );
 };
