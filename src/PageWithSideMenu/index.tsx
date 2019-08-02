01c610642e82e6917f0ae19ce5baff8cd028909d
Anton Berezin <gurunars@gmail.com>
2019-08-02 17:04:12 +0300

    Icon animations work as expected

--- src/PageWithSideMenu/index.tsx
+++ src/PageWithSideMenu/index.tsx
@@ -7,6 +7,7 @@ import responsive from "../Responsive";
 
 import ActionIcon from "../ActionIcon";
 
+import { ReactComponent as Close } from "./icons/close.svg";
 import { ReactComponent as Menu } from "./icons/menu.svg";
 
 interface Props {
@@ -46,7 +47,7 @@ const KeyTransition = ({ from, to }: { from: number; to: number }) => ({
   type: "keyframes",
   values: [from, Math.abs(from - to) / 2, to],
   times: [0, 0.5, 1],
-  duration: 400
+  duration: 500
 });
 
 const Rotatable = posed.div({
@@ -62,25 +63,59 @@ const Rotatable = posed.div({
   }
 });
 
-const Mobile = (props: Props & MenuVisibility): JSX.Element => (
-  <FullSize style={{ overflow: "hidden" }}>
-    <FullSize style={{ overflowY: "auto", display: "initial" }}>
-      {props.menuIsVisible.get() ? props.menu : props.children}
-    </FullSize>
+class MobileClass extends React.Component<Props & MenuVisibility> {
+  state: {
+    actualIsOpen: boolean;
+  };
 
-    <Rotatable
-      style={{
-        position: "absolute",
-        bottom: 20,
-        right: 20
-      }}
-      onClick={() => props.menuIsVisible.set(!props.menuIsVisible.get())}
-      pose={props.menuIsVisible.get() ? "open" : "closed"}
-    >
-      <ActionIcon onClick={() => {}} icon={<Menu />} />
-    </Rotatable>
-  </FullSize>
-);
+  constructor(props: Props & MenuVisibility) {
+    super(props);
+    this.state = {
+      actualIsOpen: props.menuIsVisible.get()
+    };
+  }
+
+  get desiredIsOpen() {
+    return this.props.menuIsVisible.get();
+  }
+
+  render(): JSX.Element {
+    const props = this.props;
+
+    return (
+      <FullSize style={{ overflow: "hidden" }}>
+        <FullSize style={{ overflowY: "auto", display: "initial" }}>
+          {this.desiredIsOpen ? props.menu : props.children}
+        </FullSize>
+
+        <Rotatable
+          style={{
+            position: "absolute",
+            bottom: 20,
+            right: 20
+          }}
+          onClick={() => props.menuIsVisible.set(!this.desiredIsOpen)}
+          pose={this.desiredIsOpen ? "open" : "closed"}
+          onValueChange={{
+            scale: it => {
+              const isInvisible = it.toFixed(2) == 0.0;
+              if (isInvisible) {
+                this.setState({ actualIsOpen: this.desiredIsOpen });
+              }
+            }
+          }}
+        >
+          <ActionIcon
+            onClick={() => {}}
+            icon={this.state.actualIsOpen ? <Close /> : <Menu />}
+          />
+        </Rotatable>
+      </FullSize>
+    );
+  }
+}
+
+const Mobile = (props: Props & MenuVisibility) => <MobileClass {...props} />;
 
 const PageWithSideMenu = responsive({
   desktopView: Desktop,
