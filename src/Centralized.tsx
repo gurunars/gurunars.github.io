97df136c170e4a23c50e85d2d0398ff4a9a599bd
Anton Berezin <gurunars@gmail.com>
2019-08-02 15:20:51 +0300

    Added stateful component

---
+++ src/Centralized.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Centralized = ({ children }: { children: any }) => (
+  <div
+    style={{
+      width: "100%",
+      height: "100%",
+      minHeight: "600px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}
+  >
+    {children}
+  </div>
+);
+
+export default Centralized;
