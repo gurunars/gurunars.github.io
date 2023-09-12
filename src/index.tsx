7dcea00cccfb65c83f0cc95569eea577d1da3b94
Anton Berezin <gurunars@gmail.com>
2023-09-13 00:37:15 +0200

    ff

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import jsyaml from 'js-yaml'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 import preprocess from './model'
@@ -7,13 +7,14 @@ import SizeAware from './SizeAware'
 
 const SITE_URL = '/portfolio.yaml'
 
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
 fetch(SITE_URL)
   .then(async item => item.text())
   .then(text =>
-    ReactDOM.render(
+    root.render(
       <SizeAware>
         <App portfolio={preprocess(jsyaml.load(text))} />
       </SizeAware>,
-      document.getElementById('root') as HTMLElement,
     ),
   )
